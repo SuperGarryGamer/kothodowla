e17dccefb0103f528f96e5a92d86256208e6604f
SuperGarryGamer <supergarrygamer@gmail.com>
2023-03-17 08:42:53 +0100

    add stackpane to catussu

--- src/com/company/Cat.java
+++ src/com/company/Cat.java
@@ -1,10 +1,17 @@
 package com.company;
 
+import javafx.scene.layout.StackPane;
+
 import java.util.LinkedList;
 
 public class Cat {
     private String name;
     private int id;
+    private StackPane stackPane;
+
+    public StackPane getStackPane() {
+        return stackPane;
+    }
 
     public int getId() {
         return id;
@@ -17,6 +24,7 @@ public class Cat {
     public Cat(String name, int id) {
         this.name = name;
         this.id = id;
+        this.stackPane = new StackPane();
     }
 
     public void addChild(Cat child) {
